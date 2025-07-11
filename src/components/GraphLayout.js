49b3ceb785b930bccdf1cdaed6e83efaedb8f3d6
Alex J. Geri <alex.222geri@gmail.com>
2025-07-10 22:42:09 -0500

    Prevenir zoom de página en móviles para que solo React Flow maneje los gestos

--- src/components/GraphLayout.js
+++ src/components/GraphLayout.js
@@ -229,8 +229,8 @@ const GraphLayout = () => {
   const selectedNode = nodes.find(n => n.id === selectedNodeId);
 
   return (
-    <div className="w-full h-full relative">
-      <div className="h-full">
+    <div className="w-full h-full relative" style={{ touchAction: 'none' }}>
+      <div className="h-full" style={{ touchAction: 'none' }}>
         <ReactFlow
           nodes={nodesWithClick}
           edges={edges}
@@ -253,6 +253,7 @@ const GraphLayout = () => {
           attributionPosition="bottom-left"
           preventScrolling={false}
           zoomOnDoubleClick={false}
+          style={{ touchAction: 'none' }}
         >
           <FlowWithFitView />
           <Controls />
